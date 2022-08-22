d9f6dc8e47a80f42a9b3b1fca3edd0486b95f62e
Bruno Peçanha <brunoitdesenv@gmail.com>
2022-08-22 12:09:51 -0300

    🐛 Corrigindo deploy do swagger no deploy

--- The3BlackBro.WebQueue.Api/Program.cs
+++ The3BlackBro.WebQueue.Api/Program.cs
@@ -41,16 +41,22 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    var _swaggerOp = new The3BlackBro.WebQueue.Api.Options.SwaggerOptions();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint(_swaggerOp.UIEndpoint, _swaggerOp.Description); ;
-        options.RoutePrefix = string.Empty;
-    });
+    app.UseSwaggerUI();
 }
 
+app.UseHsts();
+
 var swaggerOptions = new SwaggerOptions();
+var _swaggerOp = new The3BlackBro.WebQueue.Api.Options.SwaggerOptions();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint(_swaggerOp.UIEndpoint, _swaggerOp.Description); ;
+    options.RoutePrefix = string.Empty;
+});
+
 builder.Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
+
 
 app.UseRouting();
 app.UseEndpoints(endpoints =>
@@ -66,12 +72,10 @@ app.UseSwagger(options =>
 
 app.UseHttpsRedirection();
 
-// Para testes locais
 app.UseCors(x => x.AllowAnyOrigin()
                   .AllowAnyMethod()
                   .AllowAnyHeader());
 
-// Descomentar quando for utilizar autenticação
 //app.UseAuthorization();
 
 app.MapControllers();
